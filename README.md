e752673aaed9e900a142df5038e8ca8c6513614a
Dimitrios Ieronymakis <dimitris.ieronymakis@gmail.com>
2022-04-21 18:35:32 +0200

    progress on foam phantoms

--- README.md
+++ README.md
@@ -1,2 +1,32 @@
-# DART_python
+# DART - python
 DART (Discrete Algebric Reconstruction Technique) is an iterative reconstruction algorithm for discrete tomography.
+
+This repository contains an implementation of the DART algorithm, together with a framework to generate phantoms and data, to test the algorithm itself.
+
+## Prerequisites
+
+`Python 3` and the following packages are required to use this library:
+
+- `numpy`
+- `Pillow`
+- `foam_ct_phantom` : documentation is available <a href="https://github.com/dmpelt/foam_ct_phantom">here</a>.
+- `astra-toolbox` : documentation is available <a href="https://www.astra-toolbox.com/">here</a>.
+
+## Usage
+
+### Generating phantoms
+
+<b>Foam phantoms</b><br/>
+To generate foam like phantoms the library `foam_ct_phantom` was used. The file `generate_foam.py` can be used to create similar phantoms to the ones used in our experiments by running the command:
+
+```
+python generate_foam.py
+```
+
+*(a bash script to create phantoms and set arguments will be included in the future)*
+
+### Generating projection angles and detector values
+
+### Running DART
+
+## Examples and Results
---
+++ generate_foam.py
@@ -0,0 +1,38 @@
+import foam_ct_phantom
+from PIL import Image
+
+# paths and filenames
+filename = "foam_dark_2"
+phantom_save_path = 'data/foam/phantoms/'+ filename + '.h5'
+volume_save_path = "data/foam/geometries/" + filename + '.h5'
+img_save_path = "data/foam/images/" + filename + '.png'
+
+n_spheres = 1000000  # set to a low value for faster computation time
+
+# extra control variables
+random_seed = 2  # to reproduce results
+generate_foam = True
+display_img = False
+img_save = True
+
+# generate phantom
+if generate_foam:
+    foam_ct_phantom.FoamPhantom.generate(phantom_save_path,random_seed,nspheres_per_unit=n_spheres)
+# load phantom
+phantom = foam_ct_phantom.FoamPhantom(phantom_save_path)
+# generate phantom geometry
+geom = foam_ct_phantom.VolumeGeometry(256,256,1,3/256)
+# generate phantom volume from phantom geometry
+phantom.generate_volume(volume_save_path, geom)
+# load volume
+vol = foam_ct_phantom.load_volume(volume_save_path)
+
+# visualize and save image
+print(vol[0].max())
+img = Image.fromarray(vol[0]*255)
+print(vol[0].max())
+img = img.convert('L')
+if img_save:
+    img.save(img_save_path)
+if display_img:
+    img.show()
