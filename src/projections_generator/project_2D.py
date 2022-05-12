9b2efa6118d87554cb82f3022a2c1c5e116b5e9d
Dimitrios Ieronymakis <dimitris.ieronymakis@gmail.com>
2022-05-12 21:59:45 +0200

    saving in project_2D

--- src/projections_generator/project_2D.py
+++ src/projections_generator/project_2D.py
@@ -1,7 +1,11 @@
 import astra
 import numpy as np
+from os import mkdir
+from os.path import isdir
+from PIL import Image
 
-def project_from_2D(phantom_data, n_projections, detector_spacing):
+
+def project_from_2D(phantom_data, n_projections, detector_spacing, apply_noise=False, save_dir=None):
         """ Creates projection for the given input data.
             
             Parameters:
@@ -11,6 +15,9 @@ def project_from_2D(phantom_data, n_projections, detector_spacing):
                     (number of angles from where to sample measurements 
                 
                 - detector_spacing: pixel size
+
+                - save_dir: path of the directory to save image representation 
+                    of projections, when defined. To be passed as a string.
         """
 
         img_width, img_height = phantom_data.shape
@@ -29,5 +36,22 @@ def project_from_2D(phantom_data, n_projections, detector_spacing):
                                             n_detectors, angles)
         projector = astra.create_projector('line', proj_geom, vol_geom)
         proj_id, projections = astra.creators.create_sino(phantom_id, projector)
+
+        # TODO: FIX, makes pixels values only 0 or 255
+        # Apply Poisson noise.
+        if apply_noise:
+            projections = np.random.poisson(projections * 10000) / 10000
+            projections[projections > 1.1] = 1.1
+            projections /= 1.1
+
+        # Save projections if directory has been defined.
+        if save_dir != None:
+            if not isdir(save_dir):
+                mkdir(save_dir)
+            projections = np.round(projections * (2**8- 1)).astype(np.uint8)
+            for i in range(n_projections):
+                if save_dir[-1] != '/':
+                    save_dir += '/'
+                    Image.fromarray(projections[i]).save(save_dir+f'proj_{i}.png')
 
         return proj_id, projections
