392c7922ddf07a11310b1f88e1b483ff749fda98
Dimitrios Ieronymakis <dimitris.ieronymakis@gmail.com>
2022-05-13 20:08:14 +0200

    DART refactor

--- src/reconstruction_algs/DART.py
+++ src/reconstruction_algs/DART.py
@@ -4,24 +4,25 @@ class DART():
     """ gray_levels: R = {p_1, ..,  p_l}
     """
 
-    def __init__(self, gray_levels, fix_p = 0.5):
+    def __init__(self):
+        pass
+
+    def segment(self, img, gray_levels):
+        """ Segments the input image to obtain an image with
+            only the gray values specified. 
+        """
         self.gray_levels = gray_levels
         # defien thresholds for gray levels with start and end values
         self.thresholds = [0] +[(gray_levels[i]+gray_levels[i+1])/2 
                             for i in range(len(gray_levels)-1) ] + [256]
-        self.fix_p = fix_p
-
-    def segment(self, img):
-        """ Segments the input image to obtain an image with
-            only the gray values specified. 
-        """
+        # Compute segmentation
         for thresh_idx in range(len(self.thresholds)-1):
             cond = (img >= self.thresholds[thresh_idx]) * (img < self.thresholds[thresh_idx+1])
             img[cond] = self.gray_levels[thresh_idx]
         return img
 
-    def pixel_neighborhood(self, x, y):
-        """ Returns an array containing all the 
+    def pixel_neighborhood(self, img, x, y):
+        """ Returns an array containing all the neigh
         """
         # calculate all possible neighbours
         out =[]
@@ -31,10 +32,11 @@ class DART():
             out.append(np.vstack((curr_x,curr_y)).T)
         out = np.array(out)
     
+        max_x, max_y = img.shape
         # remove neighbors with invalid indexes
-        out = out[out[..., 0] >= 0]
-        out = out[out[..., 0] <= 255]
-        out = out[out[..., 1] <= 255]
+        out = out[out[..., 0] > 0]
+        out = out[out[..., 0] < max_x]
+        out = out[out[..., 1] < max_y]
         return out
 
     def boundary_pixels(self, img):
@@ -50,20 +52,20 @@ class DART():
         for x in range(img.shape[0]):
             for y in range(img.shape[1]):
                 pixel = img[x,y]
-                neighborhood_indexes = self.pixel_neighborhood(x,y)
+                neighborhood_indexes = self.pixel_neighborhood(img,x,y)
                 neighboors = img[tuple(neighborhood_indexes.T)]
                 if np.any(neighboors != pixel):
                     bool_mask[x,y] = 1
         return bool_mask
 
 
-    def non_boundary_free_pixels(self, mask, p):
+    def non_boundary_free_pixels(self, boundaries, p):
         """ Computes the non boundary free pixels of the image.
             Returns an image mask where non boundary free pixels 
             have value 1 and the rest all 0s.
             
             Parameters:
-                - mask: binary image mask as a numpy 
+                - boundaries: binary image mask as a numpy 
                         array that contains the boundary 
                         free pixels to exclude from the free pixels.
 
@@ -73,7 +75,7 @@ class DART():
 
         # we calculate the mask of possible free pixels 
         # and collapse it in 1 dimension
-        possible_pixels = np.logical_not(mask).reshape(-1)
+        possible_pixels = np.logical_not(boundaries).reshape(-1)
         # get indexes of all available pixels
         idxes = np.hstack(np.nonzero(possible_pixels))
 
@@ -87,13 +89,13 @@ class DART():
         # and substitute ones in the indexes from 'idxes' where 
         # the boolean flags are True
         free_out = np.full(fill_value=0,
-                            shape=mask.shape[0]*mask.shape[1],
+                            shape=boundaries.shape[0]*boundaries.shape[1],
                             dtype=np.uint8)
         for i in range(len(choices)):
             if choices[i] == 1:
                 free_out[idxes[i]] = 1
 
-        free_out = free_out.reshape(mask.shape)
+        free_out = free_out.reshape(boundaries.shape)
         return free_out
 
     def SART(self, W, x):
