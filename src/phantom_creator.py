50f21643301760d322f973afaa9368e0f9dd4e34
eleonoraieronimaki <eleonoraieronimaki@gmail.com>
2022-05-30 16:25:12 +0200

    binary cloud phantom

--- src/phantom_creator.py
+++ src/phantom_creator.py
@@ -223,3 +223,80 @@ def create_paws(img_size=512, gray_values=[110,120], n=1,
         if img_name != None:
             Image.fromarray(image.astype(np.uint8)).save(f"{img_name}_{i}.png") 
     return paws
+
+
+def create_clouds(img_size=512, n=1, seed=None, img_name=None):
+    """ Create cloud like phantoms.
+
+        Input:
+            - img_size: defines the size of the image,
+                should be 256 or 512.
+            - gray_values: list with three values representing the 
+                gray values to use for the images.
+            - n: defines number of images to generate.
+            - overlap: defines wether to make circles overlap or not
+            - seed: defines the seed in order to have reproducable phantoms
+            - img_name: string defining the path and filename to use.
+                The filename should not have the extension, 
+                it will be created as a png by default.
+                Example of definition of img_name:
+                    img_name = "dir/to/save/filename"
+
+        Output:
+            returns a list of phantoms. (Phantoms as numpy arrays)
+    """
+    # img size control
+    if img_size == 512:
+        x, y = 164, 164
+    elif img_size == 256:
+        x, y = 80, 80
+    else:
+        exit("img_size can only be set to 512 or 256")
+    # control seed
+    if seed != None:
+        np.random.seed(seed)
+    # define save paths
+    if img_name != None:
+        if not exists('data/'):
+            makedirs('data/')
+
+
+    clouds=[]
+    for i in range(n):
+        image, xv, yv = create_image(x,y)
+
+        noise = np.random.normal(0,0.3,3)
+
+        image[(xv-0.1)**2+(yv+0.01)**2/0.4<0.6] = 255
+        image[(xv-0.1)**2+(yv+0.5)**2/0.4<0.1] = 255
+        image[(xv-0.1)**2+(yv-0.5)**2/0.4<(0.1+noise[1])] = 255
+        image[(xv+0.4)**2+(yv+0.5)**2/0.4<0.1] = 255
+        image[(xv-0.4)**2+(yv-0.5)**2/0.4<(0.1+noise[1])] = 255
+        image[(xv-0.6)**2+(yv+0.3)**2/0.4<(0.1-noise[2])] = 255
+        image[(xv+0.4)**2+(yv-0.3)**2/0.4<(0.1-noise[2])] = 255
+        
+        n = np.random.normal(0,0.2,1)
+
+        if 0.15<n[0]>0.3 :
+            image[(xv-0.1)**2+(yv-0.1)**2/0.4<0.02] = 0
+            image[(xv+0.4)**2+(yv-0.2)**2/0.4<0.005] = 0
+            image[(xv-0.1)**2+(yv+0.2)**2/0.4<0.01] = 0
+            image[(xv-0.4)**2+(yv+0.2)**2/0.4<0.02] = 0
+            image[(xv+0.3)**2+(yv+0.3)**2/0.4<0.01] = 0
+            image[(xv-0.4)**2+(yv-0.3)**2/0.4<0.02] = 0
+        elif n[0]<0.09:
+            image[(xv+0.4)**2+(yv-0.2)**2/0.4<0.005] = 0
+            image[(xv-0.4)**2+(yv+0.2)**2/0.4<0.02] = 0
+            image[(xv-0.4)**2+(yv-0.3)**2/0.4<0.02] = 0
+            image[(xv+0.3)**2+(yv+0.3)**2/0.4<0.01] = 0
+        else: 
+            image[(xv+0.3)**2+(yv+0.3)**2/0.4<0.01] = 0
+            image[(xv-0.4)**2+(yv-0.3)**2/0.4<0.02] = 0
+            image[(xv-0.1)**2+(yv-0.1)**2/0.4<0.02] = 0
+
+        clouds.append(image)
+
+        # save image
+        if img_name != None:
+            Image.fromarray(image.astype(np.uint8)).save(f"{img_name}_{i}.png") 
+    return clouds
